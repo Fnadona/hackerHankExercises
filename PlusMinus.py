8301c5a122a2518aaeb0a1d621b9167242b87080
Nadona <felipenadona1@gmail.com>
2022-09-17 22:40:43 -0300

    plus minus solved

--- PlusMinus.py
+++ PlusMinus.py
@@ -1,1 +1,21 @@
+arr = [-1,-50, 4, 67, 0, -4, 0, 4, 5, 0]
 
+def plusMinus(arr):
+
+	countPositive = 0.000000
+	countNegative = 0.000000
+	countZero = 0.000000
+
+	for number in arr:
+		if(number > 0):
+			countPositive = countPositive + 1
+		elif (number < 0):
+			countNegative = countNegative + 1
+		else:
+			countZero = countZero + 1
+
+	print("%.6f" % (countPositive/len(arr)))
+	print("%.6f" % (countNegative/len(arr)))
+	print("%.6f" % (countZero/len(arr)))
+
+plusMinus(arr)
