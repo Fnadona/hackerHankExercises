7cc479de0357e9ff612fb37bd53ac17f34ae34ef
Nadona <felipenadona1@gmail.com>
2022-09-22 22:47:45 -0300

    Flipping the matrix solved

--- FlippingTheMatrix.py
+++ FlippingTheMatrix.py
@@ -1,60 +1,30 @@
-from QuickSort import *
-
 matrix = [[112,42,83,119],[56,125,56,49],[15,78,101,43],[62,98,114,108]]
 
-def partitionM(arr,leftMark,rightMark):
-	pivot = arr[leftMark][1]
-	count = 1
-
-	for index in range(leftMark+1, rightMark+1):
-		if(pivot > arr[index][1]):
-			temp = arr[index]
-			arr[index] = arr[leftMark + count]
-			arr[leftMark + count] = temp
-			count = count + 1
+def flippingMatrix(matrix):
 	
-	temp = arr[leftMark+count-1]
-	arr[leftMark+count-1] = arr[leftMark]
-	arr[leftMark] = temp
+	sumMatrix = 0	
 
-	return leftMark+count-1
+	for row in range(1,len(matrix)//2 + 1):
+		for col in range(1,len(matrix)//2 + 1):
+			
+			count = 1
+			maxNumber = matrix[row-1][col-1]
+		
+			while(count<4):
+				if(count == 1):
+					if(maxNumber < matrix[row-1][-col]):
+						maxNumber = matrix[row-1][-col]
+				elif(count == 2):
+					if(maxNumber < matrix[-row][col-1]):
+						maxNumber = matrix[-row][col-1]
+				else:
+					if(maxNumber < matrix[-row][-col]):
+						maxNumber = matrix[-row][-col]
+				
+				count = count + 1
+		
+			sumMatrix = sumMatrix + maxNumber
 
-def quickSortM(arr,leftMark,rightMark):
-	if(leftMark < rightMark):
-		lastPivotIndex = partitionM(arr, leftMark, rightMark)
-		quickSortM(arr,leftMark,lastPivotIndex-1)
-		quickSortM(arr,lastPivotIndex+1,rightMark)
+	return sumMatrix
 
-sumRowVector = []
-
-for row in range(len(matrix)):
-	sumRow = 0
-
-	for col in range(len(matrix)):
-		sumRow = sumRow + matrix[row][col]	
-
-	sumRowVector.append([row,sumRow])
-
-quickSortM(sumRowVector,0,len(matrix)-1)
-
-print(sumRowVector)
-print(len(matrix)-1)
-
-maxSum = []
-
-for col in range(0, len(matrix)):
-	sumCol = 0
-	
-	for index in range(len(matrix)-1,len(matrix)//2-1, -1):
-		sumCol = sumCol + matrix[sumRowVector[index][0]][col]
-
-	maxSum.append(sumCol)
-
-quickSort(maxSum,0, len(maxSum)-1)
-finalSum = 0
-for index in range(len(matrix)-1, len(matrix)//2-1, -1):
-	finalSum = finalSum + maxSum[index]
-
-print(maxSum)
-
-print(finalSum)
+print(flippingMatrix(matrix))
